8ed411d4bada6aa22450241cf1377729c34fea24
fa201 <49532187+fa201@users.noreply.github.com>
2019-09-10 19:45:45 +0200

    PV_GUI : addition of features

--- PV_GUI.py
+++ PV_GUI.py
@@ -58,24 +58,25 @@ menu_help.add_command(label='About', command=dialog_about_help)
 # ---------------------
 # Graph title area with 3 Entries
 frame_title = tk.LabelFrame(root, text='Titles for graph', labelanchor='n')
+frame_title.grid(row=0, column=0, padx=5, pady=5, stick='nesw')
 label_graph_title = tk.Label(frame_title, text='Global title')
 label_graph_title.grid(row=0, column=0, padx=5, pady=0, stick='e')
 entry_graph_title = tk.Entry(frame_title, width=20)  # add textvariable=.....
 entry_graph_title.grid(row=0, column=1, padx=5, pady=0, stick='w')
-
 label_graph_title_X = tk.Label(frame_title, text='Title for X axis')
 label_graph_title_X.grid(row=1, column=0, padx=5, pady=0, stick='e')
 entry_graph_title_X = tk.Entry(frame_title, width=20)  # add textvariable=.....
 entry_graph_title_X.grid(row=1, column=1, padx=5, pady=0, stick='w')
-
 label_graph_title_Y = tk.Label(frame_title, text='Title for Y axis')
 label_graph_title_Y.grid(row=2, column=0, padx=5, pady=0, stick='e')
 entry_graph_title_Y = tk.Entry(frame_title, width=20)  # add textvariable=.....
 entry_graph_title_Y.grid(row=2, column=1, padx=5, pady=0, stick='w')
 
+
 # Plot ranges for X and Y 
 radio_plot_scale_var = tk.IntVar()
 frame_plot_range = tk.LabelFrame(root, text='Plot ranges for X and Y axis', labelanchor='n')
+frame_plot_range.grid(row=0, column=1, padx=5, pady=5, stick='nesw')
 frame_radio_plot_range = tk.Frame(frame_plot_range)  # frame created to gather and lay the radio buttons out
 radio_plot_scale_var.set(1)
 radio_plot_scale1 = tk.Radiobutton(frame_radio_plot_range, text='Autoscale plot', variable=radio_plot_scale_var, value=1)
@@ -83,37 +84,68 @@ radio_plot_scale2 = tk.Radiobutton(frame_radio_plot_range, text='User defined', 
 radio_plot_scale1.grid(row=0, column=0, stick='w')
 radio_plot_scale2.grid(row=1, column=0, stick='w')
 frame_radio_plot_range.grid(row=0, column=0, padx=0, pady=0, columnspan=4)
-
 label_plot_user_def = tk.Label(frame_plot_range, text='User defined ranges:')
 label_plot_user_def.grid(row=1, column=0, padx=5, pady=0, columnspan=4, stick='w')
-
 label_plot_user_xmin = tk.Label(frame_plot_range, text='X min')
 label_plot_user_xmin.grid(row=2, column=0, padx=5, pady=0)
 entry_plot_user_xmin = tk.Entry(frame_plot_range, width=5)  # add textvariable=.....
 entry_plot_user_xmin.grid(row=2, column=1, padx=5, pady=0)
-
 label_plot_user_xmax = tk.Label(frame_plot_range, text='X max')
 label_plot_user_xmax.grid(row=2, column=2, padx=5, pady=0)
 entry_plot_user_xmax = tk.Entry(frame_plot_range, width=5)  # add textvariable=.....
 entry_plot_user_xmax.grid(row=2, column=3, padx=5, pady=0)
-
 label_plot_user_ymin = tk.Label(frame_plot_range, text='Y min')
 label_plot_user_ymin.grid(row=3, column=0, padx=5, pady=0)
 entry_plot_user_ymin = tk.Entry(frame_plot_range, width=5)  # add textvariable=.....
 entry_plot_user_ymin.grid(row=3, column=1, padx=5, pady=0)
-
 label_plot_user_ymax = tk.Label(frame_plot_range, text='Y max')
 label_plot_user_ymax.grid(row=3, column=2, padx=5, pady=0)
 entry_plot_user_ymax = tk.Entry(frame_plot_range, width=5)  # add textvariable=.....
 entry_plot_user_ymax.grid(row=3, column=3, padx=5, pady=0)
 
-frame_plot_custo = tk.LabelFrame(root, text='Plot customization', bg='green', labelanchor='n')
-
-# frame_curve
-frame_title.grid(row=0, column=0, padx=5, pady=5, stick='nesw')
-frame_plot_range.grid(row=0, column=1, padx=5, pady=5, stick='nesw')
+# Frame plot customization
+frame_plot_custo = tk.LabelFrame(root, text='Plot customization', labelanchor='n')
 frame_plot_custo.grid(row=0, column=2, padx=5, pady=5, stick='nesw')
+radio_plot_custo_legend = tk.IntVar()
+radio_plot_custo_legend.set(1)
+label_plot_legend = tk.Label(frame_plot_custo, text='Position of legend in the plot:')
+label_plot_legend.grid(row=0, column=0, padx=0, pady=0)
+frame_plot_legend = tk.Frame(frame_plot_custo) # frame created to gather and lay the radio buttons out
+frame_plot_legend.grid(row=1, column=0, padx=0, pady=0)
+radio_plot_legend1 = tk.Radiobutton(frame_plot_legend, text='Lower right', variable=radio_plot_custo_legend, value=1)
+radio_plot_legend2 = tk.Radiobutton(frame_plot_legend, text='Upper right', variable=radio_plot_custo_legend, value=2)
+radio_plot_legend3 = tk.Radiobutton(frame_plot_legend, text='Upper left', variable=radio_plot_custo_legend, value=3)
+radio_plot_legend4 = tk.Radiobutton(frame_plot_legend, text='Lower left', variable=radio_plot_custo_legend, value=4)
+radio_plot_legend1.grid(row=1, column=1, stick='w')
+radio_plot_legend2.grid(row=0, column=1, stick='w')
+radio_plot_legend3.grid(row=0, column=0, stick='w')
+radio_plot_legend4.grid(row=1, column=0, stick='w')
+check_display_grid_var = tk.IntVar()
+check_display_grid = tk.Checkbutton(frame_plot_custo, text='Display grid')
+check_display_grid.grid(row=2, column=0, padx=0, pady=0)
+check_display_dark_var = tk.IntVar()
+check_display_dark = tk.Checkbutton(frame_plot_custo, text='Dark background')
+check_display_dark.grid(row=3, column=0, padx=0, pady=0)
 
+# Curve management 
+frame_curve_management = tk.LabelFrame(root, text='Curve management', labelanchor='n')
+frame_curve_management.grid(row=1, column=0, padx=5, pady=5, stick='nesw', columnspan=3)
+label_curve_ID = tk.Label(frame_curve_management, text='Curve #')
+label_curve_ID.grid(row=0, column=0, padx=0, pady=0)
+label_curve_name = tk.Label(frame_curve_management, text='Curve name')
+label_curve_name.grid(row=0, column=1, padx=0, pady=0)
+label_curve_show = tk.Label(frame_curve_management, text='Show')
+label_curve_show.grid(row=0, column=2, padx=0, pady=0)
+label_curve_color = tk.Label(frame_curve_management, text='Color')
+label_curve_color.grid(row=0, column=3, padx=0, pady=0)
+label_curve_line_width = tk.Label(frame_curve_management, text='Line width')
+label_curve_line_width.grid(row=0, column=4, padx=0, pady=0)
+label_curve_line_style = tk.Label(frame_curve_management, text='Line style')
+label_curve_line_style.grid(row=0, column=5, padx=0, pady=0)
+label_curve_marker_type = tk.Label(frame_curve_management, text='Marker type')
+label_curve_marker_type.grid(row=0, column=6, padx=0, pady=0)
+button_plot = tk.Button(frame_curve_management, text='Plot the\n selected\n curves')
+button_plot.grid(row=0, column=7)
 
 
 # ------------------------------------------------------------------------------
