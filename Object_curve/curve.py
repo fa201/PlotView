75ba218bbb4f1be1ffd8e893a49e7037ef95be50
fa201 <49532187+fa201@users.noreply.github.com>
2020-03-21 17:17:12 +0100

    Set up of Curve

--- Object_curve/curve.py
+++ Object_curve/curve.py
@@ -12,45 +12,29 @@ import pandas as pd
 
 class Curve:
     """Contains all the data relative to a curve including its appearance."""
+    
+    # Class counter to specify the next available ID for a new Curve
+    next_curve_ID = 1
 
-    def __init__(self, curve_id, name, file):
+    def __init__(self):
 		# Curve ID used to handle curves
-        self.curve_id = curve_id
+        self.curve_id = Curve.next_curve_ID
 		# Curve name entered by user in PV GUI
-        self.curve_name = name  
+        self.curve_name = input("Give curve name : ")  
         # Data file opened by user in PV GUI
-        self.curve_file = file
-        # x type of data and y type of data as read in the curve file
-        self.curve_x_type = ''  
-        self.curve_y_type = ''
-		# Number of points in the curve as read in the curve file
-        self.curve_number_points = 0  
-		# dataFrame of X and Y coordinates for plot        
-        self.curve_data = DataFrame()
-		# GUI indicator to show the curve in the plot                     
-
-
-    def read_csv_file(self):
-        """Read the curve file
-
-        The file needs to be processed before reading:
-        - clean unused data
-        - rename column headers if necessary
-        - strip unwanted spaces
-        """
-
-        self.curve_data = pd.read_csv(file)
-        print("Taille des données (linges, colonnes) :", curve_data.shape, "\n")
-
+        self.curve_file = input("Give the name of data file : ")
+        # Increment class counter for next Curve creation
+        Curve.next_curve_ID +=1
 
 # ------------------------------------------------------------------------------
 # Test for Curve object. Will be removed from final script.
-curve_a = Curve(10, 'curve_named_toto')
+curve_a = Curve()
 print('Curve id: {0}'.format(curve_a.curve_id))
 print('Curve name: {0}'.format(curve_a.curve_name))
-curve_a.set_curve_type(10, 'time', 'position')
-print('Curve x type: {0}'.format(curve_a.get_curve_x_type()))
-print('Curve y type: {0}'.format(curve_a.get_curve_y_type()))
+
+curve_b = Curve()
+print('Curve id: {0}'.format(curve_b.curve_id))
+print('Curve name: {0}'.format(curve_b.curve_name))
 
 # Normal termination and free the stack.
-sys.exit(0)
+#sys.exit(0)
