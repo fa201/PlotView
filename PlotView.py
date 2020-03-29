f15482ac5f1bb0368a7786be6288949aa4d78113
fa201 <49532187+fa201@users.noreply.github.com>
2020-03-30 00:22:48 +0200

    Reorganising

--- PlotView.py
+++ PlotView.py
@@ -15,23 +15,17 @@ class Curve:
     """Contains all the data relative to a curve including its appearance."""
 
     def __init__(self, name, file):
-		# Curve name entered by user in PV GUI
-        self.c_name = name  
-        # Data file opened by user in PV GUI
-        self.c_file = file
-        # x type of data and y type of data as read in the curve file
-        self.c_x_type = ''  
+        self.c_name = name  # Curve name entered by user in PV GUI 
+        self.c_file = file  # Data file opened by user in PV GUI
+        self.c_x_type = ''  # x type of data and y type of data as read in the curve file
         self.c_y_type = ''
-		# dataFrame of X and Y coordinates for plot        
-        self.c_data = self.read_file(file)
-        # GUI indicator to show the curve in the plot                     
-        self.c_visibility = False
-		# Line color of curve -> string
-        self.c_color = 'black'  
-        self.c_width = 1.0  # line width of curve -> float
-        self.c_style = 'solid'  # line style of curve -> string
-        self.c_marker = 'circle'  # line marker (symbol) of curve -> string
-        self.c_marker_size = 1.0  # line marker size (size of symbol) of curve -> float
+        self.c_data = self.read_file(file)  # X, Y data defining the curve from file
+        self.c_visibility = False  # GUI indicator to show the curve in the plot
+        self.c_color = 'black'  # Line color of curve -> string
+        self.c_width = 1.0  # line width of curve -> float TODO: what are the limits?
+        self.c_style = 'solid'  # line style of curve -> string TODO: what are the options?
+        self.c_marker = 'circle'  # line marker (symbol) of curve -> string TODO: what are the options?
+        self.c_marker_size = 1.0  # line marker size (size of symbol) of curve -> float TODO: what are the limits?
 
     def read_file(self, file):
         """Read the curve file containing only 2 columns.
@@ -43,13 +37,12 @@ class Curve:
         - make sure that comma is the delimiter
         """
         df = pd.read_csv(file, delimiter=',')
-        print("Size of data read (lines, colums) :", df.shape, "\n")
+        print("Size of data read (lines, colums) :", df.shape)  # TODO: this should appear on status bar later
         return df
 
-
+curves = []
 
 #TODO : créer une liste de curve pour pouvoir en rajouter une automatiquement avec append.
-
 
 # Normal termination and free the stack.
 sys.exit(0)
