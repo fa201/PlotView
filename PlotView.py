ea1ff45c9523cbbe1c90e8e96ea784e31a73e600
fa201 <49532187+fa201@users.noreply.github.com>
2020-03-30 00:10:52 +0200

    Curve attributes reorganized

--- PlotView.py
+++ PlotView.py
@@ -14,65 +14,42 @@ import pandas as pd
 class Curve:
     """Contains all the data relative to a curve including its appearance."""
 
-    def __init__(self, curve_id, name, file):
-		# Curve ID used to handle curves
-        self.curve_id = curve_id
+    def __init__(self, name, file):
 		# Curve name entered by user in PV GUI
-        self.curve_name = name  
+        self.c_name = name  
         # Data file opened by user in PV GUI
-        self.curve_file = file
+        self.c_file = file
         # x type of data and y type of data as read in the curve file
-        self.curve_x_type = ''  
-        self.curve_y_type = ''
-		# Number of points in the curve as read in the curve file
-        self.curve_number_points = 0  
+        self.c_x_type = ''  
+        self.c_y_type = ''
 		# dataFrame of X and Y coordinates for plot        
-        self.curve_data = self.read_file(file)
+        self.c_data = self.read_file(file)
         # GUI indicator to show the curve in the plot                     
-        self.curve_visibility = False
+        self.c_visibility = False
 		# Line color of curve -> string
-        self.curve_line_color = 'black'  
-        self.curve_line_width = 1.0  # line width of curve -> float
-        self.curve_line_width = 'solid'  # line style of curve -> string
-        self.curve_line_marker = 'circle'  # line marker (symbol) of curve -> string
-        self.curve_line_marker_size = 1.0  # line marker size (size of symbol) of curve -> float
+        self.c_color = 'black'  
+        self.c_width = 1.0  # line width of curve -> float
+        self.c_style = 'solid'  # line style of curve -> string
+        self.c_marker = 'circle'  # line marker (symbol) of curve -> string
+        self.c_marker_size = 1.0  # line marker size (size of symbol) of curve -> float
 
     def read_file(self, file):
         """Read the curve file containing only 2 columns.
 
         The file needs to be processed before reading:
-        - clean unused data
+        - delete unused data and headers
         - rename column headers if necessary
         - strip unwanted spaces
+        - make sure that comma is the delimiter
         """
         df = pd.read_csv(file, delimiter=',')
         print("Size of data read (lines, colums) :", df.shape, "\n")
         return df
 
-#        def set_curve_type(self, curve_id, x_type, y_type):
-#        """Set the x type of data and y type of data as read in the curve file """
-#        self.curve_x_type = x_type
-#        self.curve_y_type = y_type
-
-#    def get_curve_x_type(self):
-#        """Get the x type of data as read in the curve file """
-#        return self.curve_x_type
-
-#    def get_curve_y_type(self):
-#        """Get the y type of data as read in the curve file """
-#        return self.curve_y_type
 
 
+#TODO : créer une liste de curve pour pouvoir en rajouter une automatiquement avec append.
 
-
-# ------------------------------------------------------------------------------
-# Test for Curve object. Will be removed from final script.
-curve_a = Curve(10, 'curve_named_toto')
-print('Curve id: {0}'.format(curve_a.curve_id))
-print('Curve name: {0}'.format(curve_a.curve_name))
-curve_a.set_curve_type(10, 'time', 'position')
-print('Curve x type: {0}'.format(curve_a.get_curve_x_type()))
-print('Curve y type: {0}'.format(curve_a.get_curve_y_type()))
 
 # Normal termination and free the stack.
 sys.exit(0)
