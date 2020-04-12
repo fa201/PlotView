857881dc4f75c94691b40eb0f2a669c2e4ebf556
fa201 <49532187+fa201@users.noreply.github.com>
2020-04-12 18:14:21 +0200

    Curve : deletion of unit row in CSV input files

--- PlotView.py
+++ PlotView.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """PlotView read a data file plot the data curve using matplotlib.
@@ -22,7 +21,7 @@ class Curve:
         self.c_name = name  # Curve name entered by user in PV GUI 
         self.c_file = file  # Path of data file given by user in PV GUI
         self.c_data = self.read_file(file)  # X, Y dataframe defining the curve from file
-        self.c_data_type = self.read_data_type(file)  # Dictionnary: 'x_type', 'y_type', 'x_unit', 'y_unit'
+        self.c_data_type = self.read_data_type(file)  # Dictionnary: 'x_type', 'y_type'
         self.c_visibility = False  # GUI indicator to show the curve in the plot
         self.c_color = 'black'  # Line color of curve -> string
         self.c_width = 1.0  # line width of curve -> float TODO: what are the limits?
@@ -40,40 +39,44 @@ class Curve:
         - strip unwanted spaces
         - make sure that comma is the delimiter
         """
-        df = pd.read_csv(file, delimiter=',', header=None)  # header=None to keep title and units rows
+        df = pd.read_csv(file, delimiter=',', header=0, dtype=float)  # header index=0 to skip string content. float converts data into float (necessary in order to plot)
         print(file)
         print("Size of data read (lines, colums):", df.shape)  # TODO: this should appear on status bar along with the file pat and name
-        return df.iloc[2:, :]  # Skip the first 2 rows (dat type and units)
+        #print(df.dtypes) # To confirm that the data type is float
+        print("Dataframe lu :")
+        print(df)
+        return df
 
     def read_data_type(self, file):
-        df = pd.read_csv(file, delimiter=',', header=None, nrows=2)  # Read only data type and data units
+        df = pd.read_csv(file, delimiter=',', header=None, nrows=1)  # Read only data types
         d = {}
-        d.update({'x_type': df.iloc[0, 0], 'y_type': df.iloc[0, 1],
-                'x_unit': df.iloc[1, 0], 'y_unit': df.iloc[1, 1],})
+        d.update({'x_type': df.iloc[0, 0], 'y_type': df.iloc[0, 1]})
         return d
     
-#    def plot(self):
-#        ax.plot(self.c_data.columns(0), self.c_data.columns(1))
+    def plot_df(self, axes):
+        print('plot_df')
+        #self.c_data.plot(x=self.c_data.columns[0] , y=self.c_data.iloc[:,1], ax=axes)
 
 
 # Curve list to manage the plots
 curves = []
 
 # Plot creation
-global fig
-fig = plt.Figure()
-global ax
-ax = fig.add_axes()
-#TODO débugger le tracé dans matplotlib.
+#fig, ax = plt.subplots(1)
+
 
 
 ###########TODO Test of features - TO BE REMOVED LATER
 c1 = Curve("curve 1", "test/curve1.csv")
 curves.append(c1.c_name)
-c2 = Curve("curve 2", "test/curve2.csv")
-curves.append(c2.c_name)
-print(curves)
-ax.plot(c1.c_data.iloc[:, 0], c1.c_data.iloc[:, 1])
+#c2 = Curve("curve 2", "test/curve2.csv")
+#curves.append(c2.c_name)
+#print(curves)
+
+#c1.plot_df(ax)
+#plt.draw()
+#plt.show()
+
 #######################################################
 
 
