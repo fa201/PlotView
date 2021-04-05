5f79bc17ee84a2480aa1750ebfc5c85fd4531e5d
Fabrice <49532187+fa201@users.noreply.github.com>
2021-04-05 15:31:50 +0200

    Excetion handling for import and work_dir improvements

--- PlotView_GUI.py
+++ PlotView_GUI.py
@@ -4,30 +4,51 @@
     It is the View in the Model View Controller pattern.
 """
 
-
-import sys
-import tkinter as tk
-from tkinter import font
-import webbrowser
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
+try:
+    import sys
+    import tkinter as tk
+    from tkinter import font
+    from tkinter import messagebox as msg
+    import webbrowser
+    import matplotlib.pyplot as plt
+    from matplotlib.backends.backend_tkagg import (
+        FigureCanvasTkAgg, NavigationToolbar2Tk)
+    import toto
+except ModuleNotFoundError as e:
+        print('The necessary Python packages are not installed.\n'+str(e))
+        print('Please check the required packages and their versions at https://github.com/fa201/PlotView.')
 
 
 class Gui(tk.Tk):
     """"It defines the main window of GUI."""
     def __init__(self, application):
         """Initialize the main window.
+
+        Constants:
+        - PV_VERSION: string -> plot view version as shown by git tag.
+        - WIN_RESIZABLE: boolean -> prevents the user from resizing the root window.
+        - WIN_SIZE_POS: string -> window size (width x height) and position relative to top left corner.
+        - FONT_SIZE: integer -> size of font to be used for all widget texts.
+        - PLOT_WIDTH: float -> width (in) of matplotlib figure.
+        - PLOT_HEIGHT: float -> height (in) of matplotlib figure.
         """
         super().__init__()
         # ATTRIBUTES
+        # Link to Application instance (Controller)
         self.app = application
-        # WORKING DIRECTORY TO LOOK FOR CSV FILES
-        # work_dir defines the directory for the CSV filedialog
+        self.PV_VERSION = '0.2'
+        self.WIN_RESIZABLE = False
+        self.WIN_SIZE_POS = '1280x720+0+0'
+        self.FONT_SIZE = 9
+        self.PLOT_WIDTH = 9.0
+        self.PLOT_HEIGHT = 6.68
+        # Working directory variables.
+        # 'work_dir' defines the directory for the CSV filedialog.
         self.work_dir = '___________________________________'
+        # Cut the last 35 characters of 'work_dir' so that it displays a constant width portion.
         self.work_dir_txt = tk.StringVar(self)
         # Displayed working dir path
-        self.work_dir_txt.set(self.work_dir)
+        self.work_dir_txt.set(self.work_dir[-35:-1])
 
         # METHODS
         # Allows root window to be closed by the closing icon.
@@ -44,22 +65,7 @@ class Gui(tk.Tk):
         The size cannot be changed at the moment because it is simpler.
         The default tkinter font is used with a lower size to pack more widgets.
         A status bar is created at the bottom. It shows text message through 'set_status'.
-
-        Constants:
-        - PV_VERSION: string -> plot view version as shown by git tag.
-        - WIN_RESIZABLE: boolean -> prevents the user from resizing the root window.
-        - WIN_SIZE_POS: string -> window size (width x height) and position relative to top left corner.
-        - FONT_SIZE: integer -> size of font to be used for all widget texts.
-        - PLOT_WIDTH: float -> width (in) of matplotlib figure.
-        - PLOT_HEIGHT: float -> height (in) of matplotlib figure.
         """
-        self.PV_VERSION = '0.2'
-        self.WIN_RESIZABLE = False
-        self.WIN_SIZE_POS = '1280x720+0+0'
-        self.FONT_SIZE = 9
-        self.PLOT_WIDTH = 9.0
-        self.PLOT_HEIGHT = 6.68
-
         # WINDOW
         self.title('PlotView ' + self.PV_VERSION)
         # TODO: Exception if size > size of screen and quit.
