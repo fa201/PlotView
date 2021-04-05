f3ac3ddb2636e927a9232d23c3e2f84fdcbd9df1
Fabrice <49532187+fa201@users.noreply.github.com>
2021-04-05 13:42:33 +0200

    show PV version on main window

--- PlotView.py
+++ PlotView.py
@@ -12,7 +12,7 @@ from PlotView_GUI import Gui
 from PlotView_model import Model
 
 
-class App:
+class Application:
     """ Define the controller."""
     def __init__(self):
         """ Instantiate the controller.
@@ -20,20 +20,17 @@ class App:
             Variables:
             - model: Model -> 1 instance to link to the PlotView_model file.
             - gui: GUI -> 1 instance to link to the PlotView_GUI file.
-            - pv_version: string -> plot view version ash shown by git tag.
         """
         self.model = Model()
         # The App instance is passed to the GUI instance to link both for call-backs.
         self.gui = Gui(self)
-        self.pv_version = '0.2'
 
     def main(self):
         """Launch the GUI.mainloop()"""
         self.gui.main()
 
-
 if __name__ == '__main__':
     # Create the main GUI
-    app = App()
-    # Launch the GUI mainloop which should always be the last instruction !
+    app = Application()
+    # Launch the GUI mainloop which should always be the last instruction!
     app.main()
--- PlotView_GUI.py
+++ PlotView_GUI.py
@@ -7,25 +7,58 @@
 
 import sys
 import tkinter as tk
+from tkinter import font
 
 
 class Gui(tk.Tk):
     """"It defines the main window of GUI."""
-    def __init__(self, app):
+    def __init__(self, application):
         """Initialize the main window.
-
-        The following actions are done:
-            1/ Set some constants
-            2/ Set the main GUI up
-            3/ Set the font up for all texts in widgets
-            4/ Create the GUI menus at the top
-            5/ Create the status bar at the bottom
-            6/ Set the status bar to 'Ready'
         """
         super().__init__()
-        self.app = app
+        self.app = application
         # Allows root window to be closed by the closing icon.
         self.protocol('WM_DELETE_WINDOW', self.app_quit)
+        self.window_setup()
+
+    def window_setup(self):
+        """Some basic setup is done on the GUI.
+        Title is set.
+        The size and location of the windows is set.
+        The size cannot be changed at the moment beacause it is simpler.
+        The default tkinter font is used with a lower size to pack more widgets.
+        Constants:
+        - PV_VERSION: string -> plot view version as shown by git tag.
+        - ROOT_RESIZABLE: boolean -> prevents the user from resizing the root window.
+        - ROOT_SIZE_POS: string -> Root size (width x height) and position relative to top left corner.
+        - FONT_SIZE: integer -> size of font to be used for all widget texts.
+        - PLOT_WIDTH: float -> width (in) of matplotlib figure.
+        - PLOT_HEIGHT: float -> height (in) of matplotlib figure.
+        """
+        self.PV_VERSION = '0.2'
+        self.ROOT_RESIZABLE = False
+        self.ROOT_SIZE_POS = '1280x720+0+0'
+        self.FONT_SIZE = 9
+        self.PLOT_WIDTH = 9.0
+        self.PLOT_HEIGHT = 6.68
+
+        # WINDOW
+        self.title('PlotView ' + self.PV_VERSION)
+        # TODO: Exception if size > size of screen and quit.
+        # TODO: add minimum screen size in the limitation of program in readme.
+        self.geometry(self.ROOT_SIZE_POS)
+        # Manage the size and position of root window.
+        if self.ROOT_RESIZABLE:
+            print('Warning: the main window cannot be resized.')
+        else:
+            self.resizable(0, 0)
+            # TODO: to be replaced by minsize() & maxsize() if I can handle
+            # properly the change of size in the GUI.
+        # FONT
+        my_font = font.nametofont("TkDefaultFont")
+        my_font.config(size=self.FONT_SIZE)
+        # Make my_font applicable for all widgets including menus.
+        self.option_add("*Font", my_font)
 
     def main(self):
         """Launch mainloop from App"""
