5857b23543585e5e82849995b609206b8fa1ab32
Fabrice <49532187+fa201@users.noreply.github.com>
2021-04-11 10:37:05 +0200

    font changed for all tk widgets but not ttk widgets

--- PlotView.py
+++ PlotView.py
@@ -135,20 +135,21 @@ class Application(tk.Tk):
         # Padding for all widgets inside a container
         self.WIDGET_PADX = 2
         self.WIDGET_PADY = 2
-        # Max length of string showed by 'Create curve' labels
-        self.MAX_STR_CREATE_CURVE = 32
+        # Max length of string showed by 'Create curve' labels.
+        # This is related to window width, font, and font size.
+        self.MAX_STR_CREATE_CURVE = 37
 
         # Working directory variables.
         # 'work_dir_set' defines the directory for the CSV filedialog.
         self.work_dir = ''
         self.work_dir_txt = tk.StringVar(self)
-        self.work_dir_txt.set('___________________________________')
+        self.work_dir_txt.set(self.create_underscores())
         # Path to CSV file
         # TODO: a changer et à utiliser Curve ?
         # work_file define the CSV file path
         self.work_file = ''
         self.work_file_txt = tk.StringVar(self)
-        self.work_file_txt.set('___________________________________')
+        self.work_file_txt.set(self.create_underscores())
         # Displayed working file path (only last characters.)
         #self.work_file_txt.set(self.work_file[-35:-1])
         # Curve creation label showing the curve name
@@ -163,6 +164,13 @@ class Application(tk.Tk):
         self.create_plot_area()
         self.create_notebook()
         self.tab_curve()
+
+    def create_underscores(self):
+        """Creates a string with underscores to fill the 'work_dir' and 'work file' labels when empty."""
+        underscores = list()
+        for i in range(0, self.MAX_STR_CREATE_CURVE):
+            underscores.append('_')
+        return ''.join(underscores)
 
     def window_setup(self):
         """ Some basic setup is done on the GUI.
@@ -186,8 +194,10 @@ class Application(tk.Tk):
 
         # FONT
         # https://stackoverflow.com/questions/31918073/tkinter-how-to-set-font-for-text
-        my_font = font.Font(family='TkFixedFont', size=self.FONT_SIZE)
+        my_font = font.Font(name='Courier New', size=self.FONT_SIZE)
         self.option_add("*Font", my_font)
+        # TODO: test if 'Courier New' is in font.families(). If not then provide alternative font.
+        # TODO: apply the same font and size to ttk elements (notebook, etc.) for consistency
 
         # STATUS BAR
         self.status_frame = tk.Frame(self)
