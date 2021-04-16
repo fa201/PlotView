06894ee8b6835432fa04c5ff5737b2a8d9ffbdb9
Fabrice <49532187+fa201@users.noreply.github.com>
2021-04-16 20:32:03 +0200

    help menu: link to local files for licence and help

--- PlotView.py
+++ PlotView.py
@@ -259,13 +259,13 @@ class Application(tk.Tk):
 
     def help_redirect(self):
         """ Plotview wiki is shown in web browser."""
-        webbrowser.open_new_tab('https://github.com/fa201/PlotView/wiki/Help')
+        webbrowser.open_new_tab('help/index.html')
         self.set_status('The PlotView wiki page is shown in your web browser.')
 
     def licence_redirect(self):
         """ PlotView licence is shown in the web browser."""
-        webbrowser.open_new_tab('https://github.com/fa201/PlotView/blob/master/LICENSE')
-        self.set_status('The page of GPL3 licence is shown in your web browser.')
+        webbrowser.open_new_tab('LICENSE')
+        self.set_status('The GPL3 licence is now opened.')
 
     def about_redirect(self):
         """ PlotView repository is shown in the web browser."""
