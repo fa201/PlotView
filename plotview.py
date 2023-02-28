47fd605e7452069823e29cf15b11feb2dd06dc7f
fa201 <49532187+fa201@users.noreply.github.com>
2023-02-28 21:44:13 +0100

    set up GUI look

--- plotview.py
+++ plotview.py
@@ -59,27 +59,40 @@ class Application(tk.Tk):
         super().__init__()
         self.PV_VERSION = '1.9'  # Version is positioned here to be easy to find.
         self.define_window_parameters()
-        self.setup_ttk_style()
-        self.define_fonts()
+        self.setup_GUI_look()
         # Allows root window to be closed by the closing icon.
         self.protocol('WM_DELETE_WINDOW', self.app_quit)
 
     def define_window_parameters(self):
         """Define main window parameter: title, size, position."""
         self.title(' '.join(['PlotView', self.PV_VERSION]))
-        # Set main window size and position at top left corner.
+        # Window size should be OK for most cases. Window positioned at top left corner.
         self.WIN_SIZE_POS = '1280x780+0+0'
         self.geometry(self.WIN_SIZE_POS)
 
-    def define_fonts(self):
-        """Define font for widgets
+    def setup_GUI_look(self):
+        """Define ttk style, font, button sizes 
         
-        A font is used with a lower size to pack more widgets and fixed sapcing.
-        Help:
-            https://stackoverflow.com/questions/31918073/tkinter-how-to-set-font-for-text
-            https://stackoverflow.com/questions/15462647/how-to-modify-the-default-font-in-tkinter
+        ttk style
+            - Do not work for TEntry, TCombobox
+            - Optional themes: default, clam, alt, classic
+
+        Font:
+            - A font is used with a lower size to pack more widgets and fixed spacing.
+            - Help:
+                https://stackoverflow.com/questions/31918073/tkinter-how-to-set-font-for-text
+                https://stackoverflow.com/questions/15462647/how-to-modify-the-default-font-in-tkinter
         
+        Define 3 sizes of TButtons to be used for most buttons: 4, 6, 9
         """
+        self.my_style = ttk.Style()
+        self.my_style.theme_use('alt')
+        
+        # Buttons size
+        self.my_style.configure('w4.TButton', width=4)
+        self.my_style.configure('w6.TButton', width=6)
+        self.my_style.configure('w9.TButton', width=9)
+
         self.FONT_SIZE = 9
         my_font = tk.font.nametofont('TkDefaultFont')
         my_font.configure(size=self.FONT_SIZE)
@@ -101,20 +114,6 @@ class Application(tk.Tk):
         #self.MAX_STR_CREATE_CURVE = 39
         # Number of decimals for rounding operation
         self.ROUND = 5
-
-    def setup_ttk_style(self):
-        """Set ttk parameters
-        
-        Do not work for TEntry, TCombobox
-        Optional themes: default, clam, alt, classic
-        Define 3 sizes of TButtons: 4, 6, 9
-        """
-        self.my_style = ttk.Style()
-        self.my_style.theme_use('alt')
-        # Buttons size
-        self.my_style.configure('w4.TButton', width=4)
-        self.my_style.configure('w6.TButton', width=6)
-        self.my_style.configure('w9.TButton', width=9)
 
     def app_quit(self):
             """ Quit the application and free the stack."""
